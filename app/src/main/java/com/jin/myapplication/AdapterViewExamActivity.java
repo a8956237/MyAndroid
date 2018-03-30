c85a8ff30010250b9c39cec84abb8583ea7125b8
jin <jinx94@naver.com>
2018-03-30 10:48:03 +0900

    데이터 변경 시 화면 갱신 : notifyDatasetChanged() 컨텍스트 메뉴 : 구글 검색 복 붙 registerFoncontextMenu()로 등록해야 한다

--- app/src/main/java/com/jin/myapplication/AdapterViewExamActivity.java
+++ app/src/main/java/com/jin/myapplication/AdapterViewExamActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,8 +19,10 @@ import java.util.ArrayList;
 
 public class AdapterViewExamActivity extends AppCompatActivity {
 
-    private ArrayList<People> data;
+    private ArrayList<People> mPeopleData;
     private String TAG = AdapterViewExamActivity.class.getSimpleName();
+    private PeopleAdapter mAdapter;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +30,12 @@ public class AdapterViewExamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_adapter_view_exam);
 
         // View
-        ListView listView = findViewById(R.id.list_view);
+        mListView = findViewById(R.id.list_view);
         GridView gridView = findViewById(R.id.grid_view);
         Spinner spinner = findViewById(R.id.spinner);
 
         // Data
-        data = new ArrayList<>();
+        mPeopleData = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             int picture;
             if (i % 2 == 0) {
@@ -40,22 +44,22 @@ public class AdapterViewExamActivity extends AppCompatActivity {
                 picture = R.mipmap.ic_launcher;
             }
             People people = new People("아무개 " + i, "전화번호 " + i, picture);
-            data.add(people);
+            mPeopleData.add(people);
         }
 
         // Adapter
-        PeopleAdapter adapter = new PeopleAdapter(AdapterViewExamActivity.this, data);
+        mAdapter = new PeopleAdapter(AdapterViewExamActivity.this, mPeopleData);
 
-        listView.setAdapter(adapter);
+        mListView.setAdapter(mAdapter);
 
 //        gridView.setAdapter(adapter);
 //        spinner.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//            People people = data.get(position);
-//            data.get(position);
+//            People people = mPeopleData.get(position);
+//            mPeopleData.get(position);
                 People people = (People) parent.getAdapter().getItem(position);
                 Toast.makeText(AdapterViewExamActivity.this, "그냥 클릭", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "onItemClick: " + people.toString()); // people의 정보를 본다
@@ -68,14 +72,17 @@ public class AdapterViewExamActivity extends AppCompatActivity {
             }
         });
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(AdapterViewExamActivity.this, "롱 클릭" + position, Toast.LENGTH_SHORT).show();
-                return true; // 이벤트 소비 OK
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                Toast.makeText(AdapterViewExamActivity.this, "롱 클릭" + position, Toast.LENGTH_SHORT).show();
+//                return true; // 이벤트 소비 OK
+//
+//            }
+//        });
 
-            }
-        });
+        // Context 메뉴 연결
+        registerForContextMenu(mListView);
     }
 
     @Override
@@ -104,5 +111,34 @@ public class AdapterViewExamActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_coffee, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_item1:
+                Toast.makeText(this, "액션 1", Toast.LENGTH_SHORT).show();
+                // 삭제
+                mPeopleData.remove(info.position);
+                // 업데이트
+                mAdapter.notifyDataSetChanged(); // Best!!!
+//                mListView.setAdapter(new PeopleAdapter(this, mPeopleData));
+//                mListView.setAdapter(mAdapter);
+                return true;
+            case R.id.action_item2:
+                Toast.makeText(this, "액션 2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
